c37a30869c38ec16e4f5479870eeef843798d58e
eghearn <eghearn05@gmail.com>
2023-10-12 14:46:52 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        for(int counter = 0; counter <= 30; counter++)
+        {
+            System.out.println(counter);
+        }
+        for(int counter = 30; counter >= 0; counter--)
+        {
+            System.out.println(counter);
+        }
+        for(int counter = 0; counter <= 18; counter+=3)
+        {
+            System.out.println(counter);
+        }
+        for(int counter = 10; counter >= 0; counter-=2)
+        {
+            System.out.println(counter);
+        }
+    }
+}
