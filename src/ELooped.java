515d0e6fea8199d1e5446236ec91ef2180c8c27c
eghearn <eghearn05@gmail.com>
2023-10-14 01:29:41 -0400

    c2

---
+++ src/ELooped.java
@@ -0,0 +1,60 @@
+public class ELooped
+{
+    public static void main(String[] args)
+    {
+        for(int row = 1; row <= 3; row++)
+        {
+            System.out.print("*");
+
+            for(int col = 1; col <= 24; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int row = 1; row <= 3; row++)
+        {
+            System.out.print("*");
+
+            for(int col = 1; col <= 4; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int row = 1; row <= 3; row++)
+        {
+            System.out.print("*");
+
+            for(int col = 1; col <= 18; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int row = 1; row <= 3; row++)
+        {
+            System.out.print("*");
+
+            for(int col = 1; col <= 4; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int row = 1; row <= 3; row++)
+        {
+            System.out.print("*");
+
+            for(int col = 1; col <= 24; col++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
